819d1b194f2491742634901fc32407e962344166
xinzhou-xxnr <zhouxin@xinxinnongren.com>
2016-01-26 18:49:44 +0800

    fix user verifiedTypes bug

--- controllers/9.user.js
+++ controllers/9.user.js
@@ -165,10 +165,12 @@ function process_login() {
         user.isUserInfoFullFilled = data.isUserInfoFullFilled;
         user.verifiedTypes = data.typeVerified;
         user.userTypeInName = F.global.usertypes[user.userType] || '其他';
-        user.verifiedTypesInJson = [];
-        user.verifiedTypes.forEach(function(type){
-            user.verifiedTypesInJson.push({typeId:type, typeName: F.global.usertypes[type] || '其他'});
-        });
+        if (user.verifiedTypes) {
+            user.verifiedTypesInJson = [];
+            user.verifiedTypes.forEach(function(type){
+                user.verifiedTypesInJson.push({typeId:type, typeName: F.global.usertypes[type] || '其他'});
+            });
+        }
         CartService.getOrAdd(user.userid, function(err, cart){
             if(err){
                 self.respond({code:1001, message:'获取购物车id失败'});
@@ -427,12 +429,14 @@ function json_user_get() {
         user.address = data.address;
         user.isVerified = data.isVerified;
         user.isUserInfoFullFilled = data.isUserInfoFullFilled;
-        user.verifiedTypes = data.typeVerified;
+        user.verifiedTypes = data.typeVerified || [];
         user.userTypeInName = F.global.usertypes[user.userType] || '其他';
-        user.verifiedTypesInJson = [];
-        user.verifiedTypes.forEach(function(type){
-            user.verifiedTypesInJson.push({typeId:type, typeName: F.global.usertypes[type] || '其他'});
-        });
+        if (user.verifiedTypes) {
+            user.verifiedTypesInJson = [];
+            user.verifiedTypes.forEach(function(type){
+                user.verifiedTypesInJson.push({typeId:type, typeName: F.global.usertypes[type] || '其他'});
+            });
+        }
         if (data.inviter) {
             user.inviterId = data.inviter.id;
             user.inviter = data.inviter.account;
