96b925ed044ec02e760f269ecfe1f946861398ee
xxnr-cd <cuidi@xinxinnongren.com>
2016-03-02 19:40:14 +0800

    market price added

--- public/xxnr/js/shoppingCart/shoppingCartController.js
+++ public/xxnr/js/shoppingCart/shoppingCartController.js
@@ -12,7 +12,7 @@ var app = angular.module('shop_cart', ['xxnr_common']);
 //         templateUrl: 'test.html'
 //     });
 // });
-app.controller('shoppingCartController', function($scope, $timeout, remoteApiService, commonService, loginService, sideService, shoppingCartService){
+app.controller('shoppingCartController', function($scope, $timeout, remoteApiService, commonService, loginService, sideService, shoppingCartService) {
     // $scope.$watch('$viewContentLoaded', function(event) {
     //     console.log("loaded");
     //     $footer = $(".options-box");
@@ -35,22 +35,24 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
     $scope.totalSaving = 0;
     $scope.shoppingCartCount = " ";
 
-    $scope.$on('shoppingCartController', function(e,d) {
+    $scope.$on('shoppingCartController', function(e, d) {
         //console.log(data);         //子级能得到值
         $scope.shoppingCartCount += d;
         shoppingCartService.setSCart($scope.shoppingCartCount);
         // console.log(d);
     });
 
-    if(loginService.isLogin) {
+    if (loginService.isLogin) {
         // only cart page get shoppingCart api
         if ((window.location.pathname.indexOf('cart.html') != -1) || (window.location.pathname.indexOf('confirmOrder.html') != -1)) {
             remoteApiService.getShoppingCart()
-                .then(function (data) {
+                .then(function(data) {
                     $scope.shoppingCartCount = 0;
                     if (!data || !data.datas) { // not logged on?
                         if (!data) data = {};
-                        if (!data.datas) data.datas = {"rows": []};
+                        if (!data.datas) data.datas = {
+                            "rows": []
+                        };
                     }
                     $scope.shoppingCartId = data.datas.shopCartId || data.datas.cartId;
                     for (var shopIndex in data.datas.rows) {
@@ -85,7 +87,7 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
                             item.thumbnailUrl = commonService.baseUrl + itemData.imgUrl;
                             item.onSale = (itemData.unitPrice == null || itemData.unitPrice == '') ? false : itemData.unitPrice != itemData.originalPrice;
                             item.online = itemData.online;
-                            if(!item.online){
+                            if (!item.online) {
                                 item.selected = undefined;
                             }
                             // console.log(item.online);
@@ -93,7 +95,7 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
                             // console.log(itemData.name);
                             item.additions = itemData.additions;
                             item.additionsTotalPrice = 0;
-                            for(var i in item.additions){
+                            for (var i in item.additions) {
                                 item.additionsTotalPrice = item.additionsTotalPrice + item.additions[i].price;
 
                             }
@@ -145,28 +147,28 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
                 $scope.shoppingCartCount = 0;
             }
         }
-    }else{
+    } else {
         $scope.shoppingCartCount = 0;
     }
-    $scope.selectAll = function(){
-        for(var shopIndex in $scope.shops){
-            if($scope.shops[shopIndex].allSelected == $scope.allSelected){
+    $scope.selectAll = function() {
+        for (var shopIndex in $scope.shops) {
+            if ($scope.shops[shopIndex].allSelected == $scope.allSelected) {
                 continue;
             }
             $scope.shops[shopIndex].allSelected = $scope.allSelected;
             $scope.selectShop(shopIndex);
         }
     };
-    $scope.selectShop = function(index){
+    $scope.selectShop = function(index) {
         var isAllSelected = $scope.shops[index].allSelected; //true
-        if($scope.allSelected){
+        if ($scope.allSelected) {
             $scope.allSelected = isAllSelected;
         }
 
-        for(var itemIndex in $scope.shops[index].items){
+        for (var itemIndex in $scope.shops[index].items) {
             // console.log($scope.shops[index].items[itemIndex].selected);
-            if(typeof $scope.shops[index].items[itemIndex].selected !== 'undefined'){
-                if($scope.shops[index].items[itemIndex].selected == isAllSelected){
+            if (typeof $scope.shops[index].items[itemIndex].selected !== 'undefined') {
+                if ($scope.shops[index].items[itemIndex].selected == isAllSelected) {
                     continue;
                 }
                 $scope.shops[index].items[itemIndex].selected = isAllSelected;
@@ -182,58 +184,58 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
                 break;
             }
         }
-        if(shouldModifyAll){
+        if (shouldModifyAll) {
             // console.log('test');
             $scope.allSelected = $scope.shops[index].allSelected;
             $scope.selectAll();
         }
     };
-    $scope.selectItem = function(shopIndex, itemIndex){
+    $scope.selectItem = function(shopIndex, itemIndex) {
         var isSelected = $scope.shops[shopIndex].items[itemIndex].selected;
-        if($scope.shops[shopIndex].allSelected){
+        if ($scope.shops[shopIndex].allSelected) {
             $scope.shops[shopIndex].allSelected = isSelected;
             // $scope.allSelected = isSelected;
         }
-        if($scope.allSelected){
+        if ($scope.allSelected) {
             $scope.allSelected = isSelected;
         }
         var shouldModifyShop = true;
-        for(var i in $scope.shops[shopIndex].items){
-            if($scope.shops[shopIndex].items[itemIndex].selected != $scope.shops[shopIndex].items[i].selected && $scope.shops[shopIndex].items[i].selected!==undefined){
+        for (var i in $scope.shops[shopIndex].items) {
+            if ($scope.shops[shopIndex].items[itemIndex].selected != $scope.shops[shopIndex].items[i].selected && $scope.shops[shopIndex].items[i].selected !== undefined) {
                 shouldModifyShop = false;
                 break;
             }
         }
-        if(shouldModifyShop){
+        if (shouldModifyShop) {
             $scope.shops[shopIndex].allSelected = $scope.shops[shopIndex].items[itemIndex].selected;
             $scope.selectShop(shopIndex);
         }
         calculateTotal();
     };
-    $scope.add = function(shopIndex, itemIndex){
-        if($scope.shops[shopIndex].items[itemIndex].buyCount < 9999) {
+    $scope.add = function(shopIndex, itemIndex) {
+        if ($scope.shops[shopIndex].items[itemIndex].buyCount < 9999) {
             $scope.shops[shopIndex].items[itemIndex].buyCount++;
             $scope.shops[shopIndex].totalCount++;
             $scope.buyCountChange(shopIndex, itemIndex, $scope.shops[shopIndex].items[itemIndex].buyCount, $scope.shops[shopIndex].items[itemIndex].buyCount - 1);
             submitChange($scope.shops[shopIndex].items[itemIndex].SKU_id, $scope.shops[shopIndex].items[itemIndex].buyCount);
         }
     };
-    $scope.reduce = function(shopIndex, itemIndex){
-        if($scope.shops[shopIndex].items[itemIndex].buyCount > 1) {
+    $scope.reduce = function(shopIndex, itemIndex) {
+        if ($scope.shops[shopIndex].items[itemIndex].buyCount > 1) {
             $scope.shops[shopIndex].items[itemIndex].buyCount--;
             $scope.shops[shopIndex].totalCount--;
             $scope.buyCountChange(shopIndex, itemIndex, $scope.shops[shopIndex].items[itemIndex].buyCount, $scope.shops[shopIndex].items[itemIndex].buyCount + 1);
             submitChange($scope.shops[shopIndex].items[itemIndex].SKU_id, $scope.shops[shopIndex].items[itemIndex].buyCount);
         }
     };
-    $scope.buyCountChange = function(shopIndex, itemIndex, newValue, oldValue){
+    $scope.buyCountChange = function(shopIndex, itemIndex, newValue, oldValue) {
         //if the item is not selected, select it
-        if(!$scope.shops[shopIndex].items[itemIndex].selected){
+        if (!$scope.shops[shopIndex].items[itemIndex].selected) {
             $scope.shops[shopIndex].items[itemIndex].selected = true;
             $scope.selectItem(shopIndex, itemIndex);
         }
         //if the newValue is illegal, change to 1
-        if(newValue == 0 || newValue == null || newValue == '' || parseInt(newValue) < 0 || isNaN(parseInt(newValue))){
+        if (newValue == 0 || newValue == null || newValue == '' || parseInt(newValue) < 0 || isNaN(parseInt(newValue))) {
             newValue = 1;
         }
 
@@ -245,23 +247,23 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
         $scope.shoppingCartCount += (parseInt(newValue) - parseInt(oldValue));
     };
 
-    var calculateItemPrice = function(shopIndex, itemIndex){
+    var calculateItemPrice = function(shopIndex, itemIndex) {
         var item = $scope.shops[shopIndex].items[itemIndex];
         $scope.shops[shopIndex].items[itemIndex].totalPrice = Number((item.buyCount * item.nowPrice).toFixed(2));
-        $scope.shops[shopIndex].items[itemIndex].totalDeposit = Number((item.buyCount * Number(item.deposit?item.deposit:item.nowPrice)).toFixed(2));
+        $scope.shops[shopIndex].items[itemIndex].totalDeposit = Number((item.buyCount * Number(item.deposit ? item.deposit : item.nowPrice)).toFixed(2));
     };
 
-    var calculateTotal = function(){
+    var calculateTotal = function() {
         var totalCount = 0;
         var totalPrice = 0;
         var totalSaving = 0;
-        for( var i in $scope.shops){
-            for(var j in $scope.shops[i].items){
+        for (var i in $scope.shops) {
+            for (var j in $scope.shops[i].items) {
                 var item = $scope.shops[i].items[j];
-                if(item.selected){
-                    totalPrice = Number(totalPrice) +  item.buyCount * Number(item.deposit?item.deposit:item.nowPrice);
+                if (item.selected) {
+                    totalPrice = Number(totalPrice) + item.buyCount * Number(item.deposit ? item.deposit : item.nowPrice);
                     totalCount += item.buyCount;
-                    totalSaving +=  item.buyCount * (item.oldPrice - item.nowPrice);
+                    totalSaving += item.buyCount * (item.oldPrice - item.nowPrice);
                 }
             }
         }
@@ -271,10 +273,10 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
         $scope.totalSaving = totalSaving.toFixed(2);
     };
 
-    var submitChange = function(SKU_id, newCount){
+    var submitChange = function(SKU_id, newCount) {
         // remoteApiService.changeCartNum(goodId, newCount);
         remoteApiService.changeCartNum(SKU_id, newCount)
-            .then(function (data) {
+            .then(function(data) {
                 if (data && data.code == 1000) {
                     // set shoppingCartCount
                     shoppingCartService.setSCart($scope.shoppingCartCount);
@@ -282,42 +284,42 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
             });
     };
     $scope.submitChange = submitChange;
-    $scope.deleteItem = function(shopIndex, itemIndex){
+    $scope.deleteItem = function(shopIndex, itemIndex) {
         $scope.shoppingCartCount -= $scope.shops[shopIndex].items[itemIndex].buyCount;
         submitChange($scope.shops[shopIndex].items[itemIndex].SKU_id, 0);
         $scope.shops[shopIndex].items.splice(itemIndex, 1);
-        if($scope.shops[shopIndex].items.length==0){
+        if ($scope.shops[shopIndex].items.length == 0) {
             $scope.shops.splice(shopIndex, 1);
         }
         calculateTotal();
     };
-    $scope.accessShoppingCart = function(){
+    $scope.accessShoppingCart = function() {
         commonService.accessShoppingCart();
     };
-    $scope.buy = function(){
+    $scope.buy = function() {
 
         var SKUs = [];
 
-        for(var shopIndex = 0; shopIndex<$scope.shops.length; shopIndex++){
-            for(var itemIndex = 0; itemIndex < $scope.shops[shopIndex].items.length; itemIndex++){
-                if($scope.shops[shopIndex].items[itemIndex].selected){
-					var SKU = {};
+        for (var shopIndex = 0; shopIndex < $scope.shops.length; shopIndex++) {
+            for (var itemIndex = 0; itemIndex < $scope.shops[shopIndex].items.length; itemIndex++) {
+                if ($scope.shops[shopIndex].items[itemIndex].selected) {
+                    var SKU = {};
                     SKU._id = $scope.shops[shopIndex].items[itemIndex].SKU_id;
                     SKU.count = $scope.shops[shopIndex].items[itemIndex].buyCount;
-					SKUs.push(SKU);
+                    SKUs.push(SKU);
                 }
             }
         }
-        if(!$scope.$parent.selectedAddressId || $scope.$parent.selectedAddressId==''){
+        if (!$scope.$parent.selectedAddressId || $scope.$parent.selectedAddressId == '') {
             sweetalert("请填写您的收货地址");
             return;
         }
         remoteApiService.addOrder($scope.shoppingCartId, $scope.$parent.selectedAddressId, SKUs, 1)
-            .then(function(datas){
+            .then(function(datas) {
                 $scope.data = datas;
-                if(datas.code == 1000) {
+                if (datas.code == 1000) {
                     var commitUrl = "commitPay.html?";
-                    for(var orderIndex in datas.orders){
+                    for (var orderIndex in datas.orders) {
                         commitUrl = commitUrl + "id" + '=' + datas.orders[orderIndex].id + '&';
                         // set shoppingCartCount
                         if (datas.orders[orderIndex] && datas.orders[orderIndex].SKUs) {
@@ -333,21 +335,20 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
                 }
             });
     };
-    $scope.checkout = function(checkoutPage){
+    $scope.checkout = function(checkoutPage) {
 
-        if($scope.shoppingCartCount == 0){
+        if ($scope.shoppingCartCount == 0) {
             sweetalert("购物车是空的,快去采购");
-        }else if($scope.totalCount==0){
+        } else if ($scope.totalCount == 0) {
             sweetalert("请至少选中一件商品进行结算");
-        }
-        else{
+        } else {
             // console.log( $scope.shoppingCartCount);
             var products = [];
             // console.log($scope.shoppingCartCount);
-            for(var shopIndex = 0; shopIndex<$scope.shops.length; shopIndex++){
-                for(var itemIndex = 0; itemIndex < $scope.shops[shopIndex].items.length; itemIndex++){
-                    if($scope.shops[shopIndex].items[itemIndex].selected){
-                        var product={};
+            for (var shopIndex = 0; shopIndex < $scope.shops.length; shopIndex++) {
+                for (var itemIndex = 0; itemIndex < $scope.shops[shopIndex].items.length; itemIndex++) {
+                    if ($scope.shops[shopIndex].items[itemIndex].selected) {
+                        var product = {};
                         product.id = $scope.shops[shopIndex].items[itemIndex].SKU_id;
                         product.count = $scope.shops[shopIndex].items[itemIndex].buyCount;
                         products.push(product);
@@ -360,23 +361,24 @@ app.controller('shoppingCartController', function($scope, $timeout, remoteApiSer
 
         }
     };
-    $scope.onlyHasOffline = function(index){
+    $scope.onlyHasOffline = function(index) {
         var result = true;
-        for(var itemIndex in $scope.shops[index].items){
+        for (var itemIndex in $scope.shops[index].items) {
             // console.log($scope.shops[index].items[itemIndex].selected);
-            if(typeof $scope.shops[index].items[itemIndex].selected !== 'undefined'){
+            if (typeof $scope.shops[index].items[itemIndex].selected !== 'undefined') {
                 return false;
             }
         }
         return result;
     };
-    $scope.onlyHasOfflineShop = function(){
+    $scope.onlyHasOfflineShop = function() {
         var result = true;
-        for(var shopIndex in $scope.shops){
-            if(!$scope.onlyHasOffline(shopIndex)){
+        for (var shopIndex in $scope.shops) {
+            if (!$scope.onlyHasOffline(shopIndex)) {
                 return false;
             }
         }
         return result;
     };
+
 });
