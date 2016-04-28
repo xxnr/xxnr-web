0abddd49300cd026047d97281c741dddd8a18ca4
xinzhou-xxnr <zhouxin@xinxinnongren.com>
2016-04-28 14:26:04 +0800

    add get EPOS RSC list and add Umeng IOS push message

--- configuration/umeng_config.js
+++ configuration/umeng_config.js
@@ -2,6 +2,7 @@
  * Created by zhouxin on 2016/03/18.
  */
 const pay = 1,  ziti = 2, zitiDone = 3, delivery = 4;
+const userOrderDetail = 'userOrderDetail', RSCOrderDetail = 'RSCOrderDetail';
 var umengConfig = {
 	host: "msg.umeng.com",
 	uploadPath: "/upload",
@@ -21,7 +22,8 @@ umengConfig.body[pay] = {
             'activity': "MyOrderDetailActivity",
             'ticker': "您有一笔订单未完成，请尽快支付",
             'title': "有商品到服务站啦！",
-            'text': "您订单中的商品已到服务站，请尽快完成付款"
+            'text': "您订单中的商品已到服务站，请尽快完成付款",
+            'IOSpage': userOrderDetail
         }
 };
 umengConfig.body[ziti] = {
@@ -30,14 +32,16 @@ umengConfig.body[ziti] = {
         'activity': "MyOrderDetailActivity",
         'ticker': "您的商品可以自提啦！",
         'title': "商品可以自提啦！",
-        'text': "您订单中的商品已到服务站，请到网点自提"
+        'text': "您订单中的商品已到服务站，请到网点自提",
+        'IOSpage': userOrderDetail
     },
     'RSC': {
         'after_open': "go_activity",
         'activity': "RscOrderDetailActivity",
         'ticker': "您有一笔订单可通知客户自提",
         'title': "请提前准备自提商品",
-        'text': "您网点有商品可通知客户自提，点击查看详情"
+        'text': "您网点有商品可通知客户自提，点击查看详情",
+        'IOSpage': RSCOrderDetail
     }
 };
 umengConfig.body[zitiDone] = {
@@ -46,7 +50,8 @@ umengConfig.body[zitiDone] = {
         'activity': "MyOrderDetailActivity",
         'ticker': "您有商品自提成功！",
         'title': "商品自提完成",
-        'text': "您订单中的有商品自提成功，点击查看详情"
+        'text': "您订单中的有商品自提成功，点击查看详情",
+        'IOSpage': userOrderDetail
     }
 };
 umengConfig.body[delivery] = {
@@ -55,7 +60,8 @@ umengConfig.body[delivery] = {
         'activity': "RscOrderDetailActivity",
         'ticker': "您有一笔订单中的商品待配送",
         'title': "请尽快配送商品",
-        'text': "您网点有商品可配送，点击查看详情"
+        'text': "您网点有商品可配送，点击查看详情",
+        'IOSpage': RSCOrderDetail
     }
 };
 umengConfig.retryCodes = [2028, 4000, 4001, 4002, 4009, 4021, 4023];
