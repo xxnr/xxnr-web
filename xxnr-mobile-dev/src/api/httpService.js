b8fe99cb720d3d28c3d296e0dc26df535b549ee4
xxnr-cd <cuidi@xinxinnongren.com>
2016-05-03 11:19:48 +0800

    remodule the http service

---
+++ xxnr-mobile-dev/src/api/httpService.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+// HTTP相关
+//Vue.http.options.crossOrigin = true;
+//Vue.http.options.xhr = {withCredentials: true};
+//Vue.http.options.jsonp = {'jsonp': 'callback'};
+//Vue.http.options.headers={
+//  'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+//};
+//// post的时候会把JSON对象转成formdata
+//Vue.http.options.emulateJSON=true;
+
+Vue.use(VueResource);
+Vue.http.options.xhr = {withCredentials: true};
+export const jsonpGet = (url,params,cb,errCb) =>{
+  Vue.http.jsonp(url).then(cb,errCb);
+}
--- xxnr-mobile-dev/src/api/remoteHttpApi.js
+++ xxnr-mobile-dev/src/api/remoteHttpApi.js
@@ -1,22 +1,9 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
 import {API_ROOT} from '../../config'
+import {jsonpGet} from './httpService'
 
-Vue.use(VueResource);
-
-// HTTP相关
-//Vue.http.options.crossOrigin = true;
-//Vue.http.options.xhr = {withCredentials: true};
-//Vue.http.options.jsonp = {'jsonp': 'callback'};
-//Vue.http.options.headers={
-//  'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
-//};
-//// post的时候会把JSON对象转成formdata
-//Vue.http.options.emulateJSON=true;
-Vue.http.options.xhr = {withCredentials: true};
 export default {
   getCategories: function (cb,errCb) {
-    Vue.http.jsonp(API_ROOT + 'api/v2.0/products/categories/').then(cb,errCb);
+    jsonpGet(API_ROOT + 'api/v2.0/products/categories/',null,cb,errCb);
   }
 }
 
