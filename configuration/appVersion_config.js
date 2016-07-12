c9f26a6c98be07c0e52fd608c0cae0d7425f438c
xinzhou-xxnr <zhouxin@xinxinnongren.com>
2016-07-12 19:44:06 +0800

    fix app upgrade function

---
+++ configuration/appVersion_config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zhouxin on 2016/07/12.
+ */
+var umengConfig = {
+	//Android IOS app最新版本号
+	nowIosVersion: '2.2.1',
+    nowAndroidVersion: '2.3.1'
+};
+
+umengConfig.version_map = {
+	'android': {
+		'2.3.1': '1.积分商城全新上线，精美礼品等你兑换\n2.签到规则改装升级，连续签到积分翻倍\n3.完成订单奖励积分，还能兑换礼品呦'
+	},
+	'ios': {
+		'2.2.1': '1.新增配送方式，购车可自选网点提车\n2.网点支持付现金或POS机刷卡，支付更安心\n3.自提订单凭自提码至网点，提车有保障',
+		'2.2.2': '新新农人已升级版本，快去更新应用吧~'
+	}
+};
+
+module.exports = umengConfig;
