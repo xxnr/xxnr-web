a38d70ed19ac4a8c83e36ba8fcd187e91714ac95
xxnr-cd <cuidi@xinxinnongren.com>
2016-08-02 17:03:50 +0800

    add zepto && rename folder

--- public/xxnr-mobile-dev/mobileRelease.sh
+++ public/xxnr-mobile-dev/mobileRelease.sh
@@ -30,8 +30,7 @@ mkdir -p $DIR/xxnrMobilePro/images
 ln -s ../../../xxnr/images/banners $DIR/xxnrMobilePro/images
 
 echo "-------- mkdir & ln xxnr->staticPage ... time:$now_time"
-mkdir -p $DIR/xxnrMobilePro/staticPage
-ln -s ../../../staticPage $DIR/xxnrMobilePro/
+ln -s ../../staticPage $DIR/xxnrMobilePro
 
 echo "-------- mkdir & ln xxnr->xxnrMobilePro ... time:$now_time"
 cd $DIR/..
